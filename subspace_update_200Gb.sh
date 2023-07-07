2e8efdd97ddc07aeb4f1e7fb0400e1173508de38
Artur <arthur.escada.20@gmail.com>
2023-07-07 10:40:35 +0300

    Create subspace_update_200Gb.sh

---
+++ subspace_update_200Gb.sh
@@ -0,0 +1,125 @@
+#function
+
+center()
+{ 
+IFS=""
+while read L
+do
+printf "%b\n" $(printf "%.$((($(tput cols)-${#L})/2))d" 0 | sed 's/0/ /g')$L
+done
+}
+
+function colors {
+  GREEN="\e[32m"
+  NORM="\e[0m"
+}
+
+function logo {
+bash <(curl -s https://raw.githubusercontent.com/Pandionidae/Additional_files/main/logo.sh)
+}
+
+function line {
+  echo "--------------------------------------------------------------------------------"
+}
+
+
+
+function get_vars {
+  export CHAIN="gemini-3e"
+  echo $CHAIN
+  export RELEASE="gemini-3e-2023-jul-03"
+  echo $RELEASE
+  export SUBSPACE_NODENAME=$(cat $HOME/subspace_docker/docker-compose.yml | grep "\-\-name" | awk -F\" '{print $4}')
+  echo $SUBSPACE_NODENAME
+  export WALLET_ADDRESS=$(cat $HOME/subspace_docker/docker-compose.yml | grep "\-\-reward-address" | awk -F\" '{print $4}')
+  echo $WALLET_ADDRESS
+  export PLOT_SIZE=$(cat $HOME/subspace_docker/docker-compose.yml | grep "\-\-plot-size" | awk -F\" '{print $4}')
+  echo $PLOT_SIZE
+}
+
+function delete_old {
+  docker-compose -f $HOME/subspace_docker/docker-compose.yml down -v 
+  docker volume rm subspace_docker_subspace-farmer subspace_docker_subspace-node
+}
+
+function eof_docker_compose {
+  mkdir -p $HOME/subspace_docker/
+  sudo tee <<EOF >/dev/null $HOME/subspace_docker/docker-compose.yml
+  version: "3.7"
+  services:
+    node:
+      image: ghcr.io/subspace/node:$RELEASE
+      volumes:
+        - node-data:/var/subspace:rw
+      ports:
+        - "0.0.0.0:32333:30333"
+        - "0.0.0.0:32433:30433"
+      restart: unless-stopped
+      command: [
+        "--chain", "$CHAIN",
+        "--base-path", "/var/subspace",
+        "--execution", "wasm",
+        "--blocks-pruning", "archive",
+        "--state-pruning", "archive",
+        "--port", "30333",
+        "--unsafe-rpc-external",
+        "--dsn-listen-on", "/ip4/0.0.0.0/tcp/30433",
+        "--rpc-cors", "all",
+        "--rpc-methods", "safe",
+        "--dsn-disable-private-ips",
+        "--no-private-ipv4",
+        "--validator",
+        "--name", "$SUBSPACE_NODENAME",
+        "--telemetry-url", "wss://telemetry.subspace.network/submit 0",
+        "--out-peers", "100"
+      ]
+      healthcheck:
+        timeout: 5s
+        interval: 30s
+        retries: 5
+
+    farmer:
+      depends_on:
+        - node
+      image: ghcr.io/subspace/farmer:$RELEASE
+      volumes:
+        - farmer-data:/var/subspace:rw
+      ports:
+        - "0.0.0.0:32533:30533"
+      restart: unless-stopped
+      command: [
+        "--base-path", "/var/subspace",
+        "farm",
+        "--disable-private-ips",
+        "--node-rpc-url", "ws://node:9944",
+        "--listen-on", "/ip4/0.0.0.0/tcp/30533",
+        "--reward-address", "$WALLET_ADDRESS",
+        "--plot-size", "$PLOT_SIZE"
+      ]
+  volumes:
+    node-data:
+    farmer-data:
+EOF
+}
+
+
+function update_subspace {
+  cd $HOME/subspace_docker/
+  docker-compose down
+  eof_docker_compose
+  docker-compose pull
+  docker-compose up -d
+}
+
+colors
+line | center
+logo
+line | center
+get_vars
+line | center
+delete_old
+line | center
+update_subspace
+line | center
+echo -e "${GREEN}---Оновлення завершене---${NORMAL}" | center
+cd $HOME
